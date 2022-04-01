21b6047180ec6d8d4d51c5680ff638ebb227da3e
goldcollar95 <98082293+goldcollar95@users.noreply.github.com>
2022-04-02 01:29:03 +0900

    Dynamic/실버3/30

---
+++ Dynamic program/피보나치1003.py
@@ -0,0 +1,13 @@
+#https://www.acmicpc.net/problem/1003
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    zero = 1
+    one = 0
+    tmp = 0
+    for _ in range(n):
+        tmp = one
+        one = one + zero
+        zero = tmp
+    print(zero, one) 
